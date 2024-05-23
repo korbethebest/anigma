15b0132198bf3feb94a65e093f458b497d171cf8
Dohyun Lee <dohyun@Dohyunui-noteubug.local>
2024-05-23 14:01:34 +0900

    Chore: Setup for adapting cross-env

--- main.ts
+++ main.ts
@@ -1,24 +1,35 @@
-// main.ts
 import { app, BrowserWindow } from "electron";
+import * as path from "path";
+import * as url from "url";
+
+const BASE_URL = "http://localhost:5173";
+const env = process.env.NODE_ENV || "development";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 let mainWindow: BrowserWindow | null;
 
-function createWindow() {
+function createMainWindow() {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    useContentSize: true,
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: true,
+    },
   });
 
-  mainWindow.loadURL("http://localhost:5173");
-  mainWindow.webContents.openDevTools();
-
-  mainWindow.on("closed", () => {
-    mainWindow = null;
-  });
+  if (env === "development") {
+    mainWindow.loadURL(BASE_URL);
+    mainWindow.webContents.openDevTools({ mode: "detach" });
+  } else {
+    mainWindow.loadFile(path.join(__dirname, "./index.html")); //
+  }
+  mainWindow.on("closed", () => (mainWindow = null));
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(createMainWindow);
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
@@ -28,6 +39,6 @@ app.on("window-all-closed", () => {
 
 app.on("activate", () => {
   if (mainWindow == null) {
-    createWindow();
+    createMainWindow();
   }
 });
